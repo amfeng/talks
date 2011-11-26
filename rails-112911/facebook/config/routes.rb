9b985a03cdfa86c124ea781aa31d16946527d9c6
Amber Feng <amber.feng@gmail.com>
2011-11-25 16:59:41 -0800

    added user auth

--- rails-112911/facebook/config/routes.rb
+++ rails-112911/facebook/config/routes.rb
@@ -1,6 +1,10 @@
 Facebook::Application.routes.draw do
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
+  match "register" => "users#new", :as => :register
+  match "login" => "user_sessions#new", :as => :login
+  match "logout" => "user_sessions#destroy", :as => :logout
+
+  resources :users
+  resources :user_sessions
 
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
@@ -46,9 +50,7 @@ Facebook::Application.routes.draw do
   #     resources :products
   #   end
 
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "home#index"
 
   # See how all your routes lay out with "rake routes"
 
