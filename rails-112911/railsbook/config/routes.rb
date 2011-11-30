95dd3d6420808d458bede547c07b35cd57599e71
Jonathan Ko <jonathan.ko@berkeley.edu>
2011-11-29 17:01:05 -0800

    Add some FIXMEs

--- rails-112911/railsbook/config/routes.rb
+++ rails-112911/railsbook/config/routes.rb
@@ -1,13 +1,18 @@
 Railsbook::Application.routes.draw do
 
   # Registration & sessions
+
+  # FIXME change /register to /signup
   get   "register" => "users#new",             :as => :register
   post  "register" => "users#create"
+
   get   "login"    => "user_sessions#new",     :as => :login
   match "logout"   => "user_sessions#destroy", :as => :logout
 
   post "friend" => "users#friend", :as => :friend
   post "unfriend" => "users#unfriend", :as => :unfriend
+
+  # FIXME Add routes for comments
 
   get   '/' => 'home#index', :as => :home
 
