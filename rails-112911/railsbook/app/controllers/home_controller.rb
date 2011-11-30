235e5f1c2ecfc77d1ee8806817c7207496cdfc2f
Jonathan Ko <jonathan.ko@berkeley.edu>
2011-11-29 16:31:10 -0800

    Dumb down eager loading, remove edit profile link

--- rails-112911/railsbook/app/controllers/home_controller.rb
+++ rails-112911/railsbook/app/controllers/home_controller.rb
@@ -4,8 +4,9 @@ class HomeController < ApplicationController
       @users = User.all
       @users.delete current_user
 
-      # FIXME(Jon): Change to friends' posts, sorted by time
-      @posts = ( [@current_user] | @current_user.friends).collect(&:posts).flatten.sort_by(&:created_at)
+      friends = @current_user.friends
+
+      @posts = ( [@current_user] | friends).collect(&:posts).flatten.sort_by(&:created_at)
       @posts.reverse!
       render :index
     else
